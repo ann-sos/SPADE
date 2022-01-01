256d07bd80778a7e357e004647a7b6df218b9571
ann-sos <anna.w.sosulska@gmail.com>
2022-01-01 19:35:55 +0100

    Add find_F1 function for finding frequent 1-sequences.

--- src/SPADE/spade.py
+++ src/SPADE/spade.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def read_dataset(path: str) -> pd.DataFrame:
     """
     This function reads into pandas DataFrame a dataset formatted as follows:
@@ -19,14 +20,22 @@ def read_dataset(path: str) -> pd.DataFrame:
                     data.append((sid, eid, item))
                 eid += 1
             sid += 1
-    df = pd.DataFrame(data, columns=['SID', 'EID', 'Items'])
     return pd.DataFrame(data, columns=['SID', 'EID', 'Items'])
 
 
-def spade():
-    # 1 Find frequent items or 1-sequences e.g. {A}, {B} 
-    #   1a find atoms
-    #   1b keep only the frequent ones
+def find_F1(df: pd.DataFrame, min_sup: int) -> pd.DataFrame:
+    """
+    Finds frequent (support >= min_sup) items or 1-sequences,
+    e.g. {A}, {B}, returns dataframe with these items and their support.
+    """
+    atoms_series = df.groupby('Items')['SID'].nunique()
+    atoms = pd.DataFrame({'Items': atoms_series.index, 'Support': atoms_series.values})
+    return atoms.loc[atoms['Support'] >= min_sup]
+
+
+def spade(df: pd.DataFrame, min_sup: int):
+    # (STEP 1): Find atoms and their support
+    F1 = find_F1(df, min_sup)
     # 2 Find frequent 2-sequences (containing 2 items) e.g. {AB}, {A}->{B}, {B}->{A}
     # 3 Find equivalence classes / sequences longer than 3 items
     # 4 Calculate support for sequences from step 3
--- tests/test_spade.py
+++ tests/test_spade.py
@@ -1,10 +1,23 @@
 import pytest
+import numpy as np
 import pandas as pd
-from SPADE.spade import read_dataset
+from SPADE.spade import read_dataset, find_F1
 
 
-def test_read_dataset():
-    df_expected = pd.DataFrame([[0, 0, 'C'], [0, 0, 'D'], [0, 1, 'A'], [0, 1, 'B'], [0, 1, 'C'], [0, 2, 'A'], [0, 2, 'B'], [0, 2, 'F'], [0, 3, 'A'], [0, 3, 'C'], [0, 3, 'D'], [0, 3, 'F'], [1, 0, 'A'], [1, 0, 'B'], [
-                               1, 0, 'F'], [1, 1, 'E'], [2, 0, 'A'], [2, 0, 'B'], [2, 0, 'F'], [3, 0, 'D'], [3, 0, 'G'], [3, 0, 'H'], [3, 1, 'B'], [3, 1, 'F'], [3, 2, 'A'], [3, 2, 'G'], [3, 2, 'H']], columns=['SID', 'EID', 'Items'])
+@pytest.fixture()
+def df_expected() -> pd.DataFrame:
+    return pd.DataFrame([[0, 0, 'C'], [0, 0, 'D'], [0, 1, 'A'], [0, 1, 'B'], [0, 1, 'C'], [0, 2, 'A'], [0, 2, 'B'], [0, 2, 'F'], [0, 3, 'A'], [0, 3, 'C'], [0, 3, 'D'], [0, 3, 'F'], [1, 0, 'A'], [1, 0, 'B'], [1, 0, 'F'], [1, 1, 'E'], [2, 0, 'A'], [2, 0, 'B'], [2, 0, 'F'], [3, 0, 'D'], [3, 0, 'G'], [3, 0, 'H'], [3, 1, 'B'], [3, 1, 'F'], [3, 2, 'A'], [3, 2, 'G'], [3, 2, 'H']], columns=['SID', 'EID', 'Items'])
+
+
+@pytest.fixture()
+def atoms_expected() -> pd.DataFrame:
+    return pd.DataFrame([['A', 4], ['B', 4], ['D', 2], ['F', 4]])
+
+
+def test_read_dataset(df_expected):
     df_returned = read_dataset('tests/test_data.txt')
     pd.testing.assert_frame_equal(df_returned, df_expected)
+
+
+def test_find_F1(atoms_expected, df_expected):
+    np.array_equal(find_F1(df_expected, 2), atoms_expected)
