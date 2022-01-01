7c6972429f58f633584d89086aa69c69d49fabed
ann-sos <anna.w.sosulska@gmail.com>
2022-01-01 18:08:07 +0100

    Refactor dataframe for easier data manipulation

--- src/SPADE/spade.py
+++ src/SPADE/spade.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
 def read_dataset(path: str) -> pd.DataFrame:
+    """
+    This function reads into pandas DataFrame a dataset formatted as follows:
+        -1 separates elements
+        -2 separates sequences
+        Item can contain one or more items"""
     sid = 0
     data = []
     with open(path, 'r') as file:
@@ -9,8 +14,20 @@ def read_dataset(path: str) -> pd.DataFrame:
             line = line.split(' -2')
             line = line[0].split(' -1 ')
             for element in line:
-                data.append((sid, eid, element))
+                element = element.split()
+                for item in element:
+                    data.append((sid, eid, item))
                 eid += 1
             sid += 1
     df = pd.DataFrame(data, columns=['SID', 'EID', 'Items'])
     return pd.DataFrame(data, columns=['SID', 'EID', 'Items'])
+
+
+def spade():
+    # 1 Find frequent items or 1-sequences e.g. {A}, {B} 
+    #   1a find atoms
+    #   1b keep only the frequent ones
+    # 2 Find frequent 2-sequences (containing 2 items) e.g. {AB}, {A}->{B}, {B}->{A}
+    # 3 Find equivalence classes / sequences longer than 3 items
+    # 4 Calculate support for sequences from step 3
+    pass
--- tests/test_spade.py
+++ tests/test_spade.py
@@ -4,9 +4,7 @@ from SPADE.spade import read_dataset
 
 
 def test_read_dataset():
-    df_expected = pd.DataFrame([[0, 0, 'C D'], [0, 1, 'A B C'], [0, 2, 'A B F'], [0, 3, 'A C D F'],
-                                [1, 0, 'A B F'], [1, 1, 'E'],
-                                [2, 0, 'A B F'],
-                                [3, 0, 'D G H'], [3, 1, 'B F'], [3, 2, 'A G H']], columns=['SID', 'EID', 'Items'])
+    df_expected = pd.DataFrame([[0, 0, 'C'], [0, 0, 'D'], [0, 1, 'A'], [0, 1, 'B'], [0, 1, 'C'], [0, 2, 'A'], [0, 2, 'B'], [0, 2, 'F'], [0, 3, 'A'], [0, 3, 'C'], [0, 3, 'D'], [0, 3, 'F'], [1, 0, 'A'], [1, 0, 'B'], [
+                               1, 0, 'F'], [1, 1, 'E'], [2, 0, 'A'], [2, 0, 'B'], [2, 0, 'F'], [3, 0, 'D'], [3, 0, 'G'], [3, 0, 'H'], [3, 1, 'B'], [3, 1, 'F'], [3, 2, 'A'], [3, 2, 'G'], [3, 2, 'H']], columns=['SID', 'EID', 'Items'])
     df_returned = read_dataset('tests/test_data.txt')
     pd.testing.assert_frame_equal(df_returned, df_expected)
