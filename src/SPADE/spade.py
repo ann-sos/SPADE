fc182361af713b9b0dbda106dd5bb5dda69f15ad
ann-sos <anna.w.sosulska@gmail.com>
2022-01-07 08:34:35 +0100

    Added find_F2 function.

--- src/SPADE/spade.py
+++ src/SPADE/spade.py
@@ -65,6 +65,42 @@ def find_F1(df: pd.DataFrame, min_sup: int) -> pd.DataFrame:
     return supports, F1
 
 
+
+def find_F2(F1: dict, supports: pd.DataFrame, min_sup: int):
+    sq2_ctr = set()
+    item_tree = {}   
+    F1_list = list(F1.keys())
+    F1_list = [eval(item) for item in F1_list]
+    #print(F1_list)
+    for i, item1 in enumerate(F1_list):
+        item_lvl_list = {}
+        for j in range(len(F1_list)):
+            item2 = F1_list[j]
+            #generate event items
+            if i != j:
+                event_item = repr([set(sorted((item1).union(item2)))])
+                #print(f"event item {event_item}")
+                if not event_item in sq2_ctr:
+                    event_item_df = event_items_join(F1[repr(item1)], F1[repr(item2)])           
+                    support_value = event_item_df['SID'].nunique()
+                    if support_value >= min_sup:
+                        supports = supports.append({'Items': event_item, 'Support': support_value}, ignore_index=True)
+                        item_lvl_list[event_item]= event_item_df
+                sq2_ctr.add(event_item)
+            #generate sequence items     
+            sq_item = repr([item1, item2])
+            #print('sq_item', sq_item)
+            if not sq_item in sq2_ctr:
+                sq_item_df = sequence_items_join(F1[repr(item1)], F1[repr(item2)])           
+                support_value = sq_item_df['SID'].nunique()
+                if support_value >= min_sup:
+                    supports = supports.append({'Items': sq_item, 'Support': support_value}, ignore_index=True)
+                    item_lvl_list[sq_item] = sq_item_df 
+            sq2_ctr.add(sq_item)
+        item_tree[repr([(set(sorted(item1)))])] = item_lvl_list
+    return supports, item_tree
+
+
 def spade(df: pd.DataFrame, min_sup: int):
     # (STEP 1): Find atoms and their support
     supports, F1 = find_F1(df, min_sup)
